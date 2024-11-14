26b332246f3d21c745e4b2327a975ead8b322b58
Nyxelle-05 <nyxiee.nyxxx@gmail.com>
2024-11-14 13:20:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Use the command /vouch , /vouchie];
+const statusMessages = ["Use the command /vouch , /vouchie"];
 const statusTypes = [ 'online'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
